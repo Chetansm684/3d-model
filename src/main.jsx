1b7c5d399ed17170e648de6fd1e8a8bd85a35f74
Chetan Meshram <meshramcm684@gmail.com>
2024-06-20 23:38:33 +0530

    init

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 //...
 import * as Sentry from "@sentry/react";
